f0f19f207798aa45d0c237bc395d9d1cd49971dd
S.Jacqueline <s.jacquelinesalas@gmail.com>
2024-04-29 14:55:52 -0700

    change founts

--- scripts/script.js
+++ scripts/script.js
@@ -6,13 +6,9 @@ const profileAbout = document.querySelector(".profile__about");
 const inputName = document.querySelector(".popup__input-name");
 const inputAbout = document.querySelector(".popup__input-about");
 const formProfile = document.querySelector(".popup__form");
-const buttonSave = document.querySelector("popup__button-save");
+const buttonSave = document.querySelector(".popup__button-save");
 
 buttonEdit.addEventListener("click", handleOpenProfile);
-
-buttonClose.addEventListener1("click", handleCloseProfile);
-
-buttonSave.addEventListener("click", handleSaveInfo);
 
 function handleOpenProfile(evt) {
   popupProfile.classList.add("popup_show");
